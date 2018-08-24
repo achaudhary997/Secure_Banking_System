d5ff3d426f5fad181d486e21b5aa4710190a9d27
YashitM <yashit16123@iiitd.ac.in>
2018-08-24 21:59:36 +0530

    login view done

--- website/views.py
+++ website/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import requests
 
-# Create your views here.
+# # Create your views here.
 def login(request):
     
-    return render(request, 'website/login.html')
+    if request.method == 'POST':
+        form = # form name
+        if form.is_valid():
+        recaptchaResponse = request.POST.get('g-recaptcha-response')
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        data = {
+            'secret' : '6LcqCWwUAAAAAC9-4iofBAthF8pwPHQlSg6n9w4O' any better place to store this?,
+            'response' : recaptchaResponse
+        }
+        r = requests.post(url, data=data)
+        result = r.json()
+        if result['success']:
+            # username password validation
+            return render(request, 'website/login.html') # Got to change this to dashboard or smth
+        else:
+            return render(request, 'website/login.html')
+            
+
+        
