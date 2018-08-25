3a054ca4ddf81c88d24dfb88cfe2f623e278ea15
brucewayne <brucewayne@localhost.localdomain>
2018-08-25 13:55:17 +0530

    removed venv

--- venv/sbs/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/brucewayne/Desktop/Deepak/IIIT/FCS/Project/Secure_Banking_System/venv/sbs/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
