6d00eeb03470dcc1b102693f9920632cb92df32b
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2018-10-02 19:18:32 +0530

    Added sober mode to deploy the website
    To run use `sober=TRUE python manage.py runserver <IP>:<PORT>`

--- secure_bank/settings.py
+++ secure_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,5 +133,10 @@ STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "static"),
 ]
 
-WEBSITE_NAME = "GoldWomanSex"
+if os.environ.get('sober') == 'TRUE':
+	WEBSITE_NAME = "GoldWomanSachs"
+else:
+	WEBSITE_NAME = "GoldWomanSex"
 
+
+# print (os.environ.get('sober'))
