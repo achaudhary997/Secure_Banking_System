18730e63694cbfb48741949c160fefdfe3a6eaea
Anubhav Chaudhary <anubhav16013@iiitd.ac.in>
2018-11-02 20:53:09 +0530

    updated requirements.txt

---
+++ runabcd.sh
@@ -0,0 +1,1 @@
+uwsgi --http :8080 --home /home/iiitd/Secure_Banking_System/venv --chdir /home/iiitd/Secure_Banking_System -w secure_bank.wsgi
--- secure_bank/settings.py
+++ secure_bank/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6(2#5b%p+bao&fbegp^#7dugg8q&mi0(_52z&vmfss9-db-@=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
